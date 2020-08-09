925ded268eba5fff872b00a067d8113c48e0d78a
dedy <dedyindra351@gmail.com>
2020-08-09 15:32:34 +0700

    improve chart view years for now

--- application/models/m_chart.php
+++ application/models/m_chart.php
@@ -2,14 +2,14 @@
 
 class M_chart extends CI_Model{
 	function tampil_chart($id){
-		$sql="SELECT monthname(jangka_waktu) as bulan,if(status ='success',sum(status = 'success'),sum(status = 'success')) as success ,if(status !='success',sum(status  !='success'),sum(status  !='success')) as work from tbl_tugas where id_tujuan = '$id'  group by month(jangka_waktu) order by month(jangka_waktu)";
+		$sql="SELECT monthname(jangka_waktu) as bulan,if(status ='success',sum(status = 'success'),sum(status = 'success')) as success ,if(status !='success',sum(status  !='success'),sum(status  !='success')) as work from tbl_tugas where id_tujuan = '$id' and YEAR(jangka_waktu)=YEAR(NOW()) group by month(jangka_waktu) order by month(jangka_waktu)";
 		$result = $this->db->query($sql);
 		return $result->result();	
 	}
 
 
 	function tampil_chart_Admin(){
-		$sql="SELECT monthname(jangka_waktu) as bulan,if(status ='success',sum(status = 'success'),sum(status = 'success')) as success ,if(status !='success',sum(status  !='success'),sum(status  !='success')) as work from tbl_tugas  group by month(jangka_waktu) order by month(jangka_waktu)";
+		$sql="SELECT monthname(jangka_waktu) as bulan,if(status ='success',sum(status = 'success'),sum(status = 'success')) as success ,if(status !='success',sum(status  !='success'),sum(status  !='success')) as work from tbl_tugas  where YEAR(jangka_waktu)=YEAR(NOW()) group by month(jangka_waktu) order by month(jangka_waktu)";
 		$result = $this->db->query($sql);
 		return $result->result();	
 	}
